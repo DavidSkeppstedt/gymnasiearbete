1b249e09be5a3708386a792c93d8e65c0600d14e
David Skeppstedt <david.skeppstedt@gmail.com>
2014-02-24 17:46:39 +0100

    Version 5, new menu

---
+++ Assets/Screens/Menu/Scripts/PointLight.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointLight : MonoBehaviour {
+
+	public GameObject pointlight;
+
+	private float time = 1;
+	private float counter = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (counter < time) {
+			counter += Time.deltaTime;
+
+		} else {
+
+
+
+			//Do stuff
+			
+			if (pointlight.activeSelf) {
+				pointlight.SetActive(false);
+			}else {
+				pointlight.SetActive(true);
+			}
+			
+			counter = 0;
+
+		}
+
+
+	
+	}
+	
+
+
+
+
+
+}
